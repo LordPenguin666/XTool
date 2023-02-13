fed9936fa85ad131238402bc5477a033b2375b13
“lordpenguin666” <40115288+LordPenguin666@users.noreply.github.com>
2023-02-13 23:30:43 +0800

    Update.

--- caddy_operation.go
+++ caddy_operation.go
@@ -1,1 +1,13 @@
 package main
+
+func (c *Config) StartCaddy() *Config {
+	return c
+}
+
+func (c *Config) EnableCaddy() *Config {
+	return c
+}
+
+func (c *Config) StopCaddy() *Config {
+	return c
+}
