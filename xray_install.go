d2b3cc2c6ff070b62a9d3ef267913046ceebf250
“lordpenguin666” <40115288+LordPenguin666@users.noreply.github.com>
2023-02-14 18:33:36 +0800

    Update.

--- xray_install.go
+++ xray_install.go
@@ -8,8 +8,9 @@ import (
 func (c *Config) XrayInstallation() *Config {
 	shell := exec.Command("bash", "-c",
 		"$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)",
-		"@", "install", "-u", "caddy",
+		"@ install -u caddy",
 	)
+	fmt.Println(shell.String())
 	output, err := shell.CombinedOutput()
 	if err != nil {
 		c.logger.Error(err.Error())
