bac5f959d351a6ce6a3bfa41178d43a69ed9aa29
“lordpenguin666” <40115288+LordPenguin666@users.noreply.github.com>
2023-02-14 18:35:47 +0800

    Update.

--- xray_install.go
+++ xray_install.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 )
 
 func (c *Config) XrayInstallation() *Config {
 	shell := exec.Command("bash", "-c",
-		"$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)",
+		"'$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)'",
 		"@ install -u caddy",
 	)
 	fmt.Println(shell.String())
+	os.Exit(0)
 	output, err := shell.CombinedOutput()
 	if err != nil {
 		c.logger.Error(err.Error())
@@ -21,7 +23,7 @@ func (c *Config) XrayInstallation() *Config {
 
 func (c *Config) XrayUninstallation() *Config {
 	shell := exec.Command("bash", "-c",
-		"$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)",
+		"'$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)'",
 		"@", "remove", "--purge",
 	)
 	output, err := shell.CombinedOutput()
